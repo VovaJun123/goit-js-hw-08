1545a15857c9d0d4da611c393a2871fa8e9705bf
VovaJun123 <121401616+VovaJun123@users.noreply.github.com>
2023-08-29 20:44:40 +0100

    four commit

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -45,7 +45,7 @@ import throttle from 'lodash.throttle';
 const LOCAL_KEY = 'feedback-form-state';
 let formData = JSON.parse(localStorage.getItem(LOCAL_KEY)) || {};
 
-form = document.querySelector('.feedback-form');
+let form = document.querySelector('.feedback-form');
 
 form.addEventListener('input', throttle(storageFormData, 500));
 form.addEventListener('submit', onFormSubmit);
